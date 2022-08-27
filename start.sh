f766cfc6b3eb92769a4a653881b6ee6a9537e182
Ömer SAVAŞ <omer.savas@hotmail.com.tr>
2022-08-27 16:05:24 +0300

    fix: change permissions and paths

--- start.sh
+++ start.sh
@@ -5,15 +5,12 @@ docker swarm init
 
 git clone https://github.com/MikroGovernment/angaryos.git
 
-cd angaryos/
+chmod 777 -R ./angaryos/services/
 
-chmod 777 -R ./services/
-chmod 777 -R angaryos/services/ftpserver/
+chmod 600 ./angaryos/services/postgresql/.pgpass
+chown 26:26 ./angaryos/services/postgresql/.pgpass
 
-chmod 600 ./services/postgresql/.pgpass
-chown 26:26 ./services/postgresql/.pgpass
-
-mkdir ./services/postgresql/data
-chmod 777 -R ./services/postgresql/data
+mkdir ./angaryos/services/postgresql/data
+chmod 777 -R ./angaryos/services/postgresql/data
 
 docker stack deploy --compose-file ./docker-stack-dev.yml angaryos
